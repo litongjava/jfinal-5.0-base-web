4e72653663bc777fdde44fd5340ab1910862b41b
litongjava <litongjava@qq.com>
2023-03-15 09:12:12 +0800

    整合fastjson

--- src/main/java/com/litongjava/jfinal/web/config/BaseConfig.java
+++ src/main/java/com/litongjava/jfinal/web/config/BaseConfig.java
@@ -13,10 +13,16 @@ public class BaseConfig extends JFinalConfig {
 
   public void configConstant(Constants me) {
     me.setDevMode(PropKitUtils.getBoolean("undertow.devMode"));
+    // 配置对超类中的属性进行注入
+    me.setInjectSuperClass(true);
+
+    // 开启解析 json 请求，5.0.0 版本新增功能
+    me.setResolveJsonRequest(true);
   }
 
   public void configRoute(Routes me) {
     me.scan("com.litongjava.jfinal.web.");
+
   }
 
   public void configEngine(Engine me) {
